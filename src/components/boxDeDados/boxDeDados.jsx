a9011ce69307f64c4d79993b8774ada010e8306b
Marcus <alpande_1904@hotmail.com>
2024-02-26 19:16:08 -0300

    Filtro de datas para o box-dados

--- src/components/boxDeDados/boxDeDados.jsx
+++ src/components/boxDeDados/boxDeDados.jsx
@@ -33,32 +33,48 @@ function BoxDeDados() {
       endDateInput.value = endDateFormatted;
     });
 
-    api.get("/box-dados")
-      .then((response) => {
-        // Atualiza o estado com os dados recebidos do backend
+    loadData(startDateFormatted, endDateFormatted);
+  }, []);
+
+  const loadData = (startDate, endDate) => {
+    api.get(`/box-dados?startDate=${startDate}&endDate=${endDate}`)
+    .then((response) => {
         setBoxDeDados(response.data);
 
-        // Itera sobre os dados recebidos para atualizar os estados correspondentes
-        response.data.forEach((item) => {
-          if (item.status_servico === 'S') {
-            setTotalAtendimentos(item.total);
-            setTotalEmRealAtendidos(item.total_valor_formatado);
+        let totalAtendimentos = 0;
+        let totalEmRealAtendidos = 0;
+        let totalCancelamentos = 0;
+        let totalEmRealCancelados = 0;
+        let totalAgendado = 0;
+        let totalEmRealAgendado = 0;
 
-          } else if (item.status_servico === 'C') {
-            setTotalCancelamentos(item.total);
-            setTotalEmRealCancelados(item.total_valor_formatado);
-          } else if (item.status_servico === 'A') {
-            setTotalAgendado(item.total);
-            setTotalEmRealAgendado(item.total_valor_formatado);
+        response.data.forEach((item) => {
+          if (item.data_servico >= startDate && item.data_servico <= endDate) {
+            if (item.status_servico === 'S') {
+              totalAtendimentos += item.total;
+              totalEmRealAtendidos = item.total_valor_formatado;
+            } else if (item.status_servico === 'C') {
+              totalCancelamentos += item.total;
+              totalEmRealCancelados = item.total_valor_formatado;
+            } else if (item.status_servico === 'A') {
+              totalAgendado += item.total;
+              totalEmRealAgendado = item.total_valor_formatado;
+            }
           }
         });
+
+        setTotalAtendimentos(totalAtendimentos);
+        setTotalEmRealAtendidos(totalEmRealAtendidos);
+        setTotalCancelamentos(totalCancelamentos);
+        setTotalEmRealCancelados(totalEmRealCancelados);
+        setTotalAgendado(totalAgendado);
+        setTotalEmRealAgendado(totalEmRealAgendado);
       })
       .catch((err) => {
-        // Trata erros caso ocorram na requisição
         console.error("Erro ao buscar os dados:", err);
         setError("Erro ao buscar os dados.");
       });
-  }, []);
+  };
 
   const handleSearch = () => {
     const startDateInputEdit = document.getElementById('startDateInput').value;
@@ -71,7 +87,7 @@ function BoxDeDados() {
       setError("A data final não pode ser anterior à data inicial.");
     } else {
       setError("");
-      console.log("Pesquisar...");
+      loadData(startDateInputEdit, endDateInputEdit);
     }
   };
 
@@ -93,8 +109,8 @@ function BoxDeDados() {
               </div>
             </div>
             <div className="col-lg-2 col-12 mb-2">
-              <button type="button" className="btn btn-primary" value="BUSCAR" onClick={handleSearch}>
-                <i className="bi bi-search"></i>
+              <button type="button" className="btn btn-primary" onClick={handleSearch}>
+                <i className="bi bi-search"></i> BUSCAR
               </button>
             </div>
           </div>
@@ -104,29 +120,36 @@ function BoxDeDados() {
       <section className="content">
         <div className="container-fluid">
           <div className="row">
-          
-              
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn btn-success flex-fill w-100" onClick={handleSearch}>Atendimentos realizados<h3>{totalAtendimentos}</h3></button>
-                </div>
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn btn-warning flex-fill w-100">Cancelamentos realizados<h3>{totalCancelamentos}</h3></button>
-                </div>
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn btn-info flex-fill w-100">Ganhos realizados<h3>{totalEmRealAtendidos}</h3></button>
-                </div>
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn bg-danger flex-fill w-100">Custos de atendimentos<h3>FAZER ...</h3></button>
-                </div>
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn bg-secondary flex-fill w-100">Serviços a fazer<h3>{totalAgendado}</h3></button>
-                </div>
-                <div className="col-sm-6 mb-2">
-                  <button type="button" className="btn bg-secondary flex-fill w-100">Ganhos esperados<h3>{totalEmRealAgendado}</h3></button>
-                </div>
-
-              
-           
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn btn-success flex-fill w-100" onClick={handleSearch}>
+                Atendimentos realizados<h3>{totalAtendimentos}</h3>
+              </button>
+            </div>
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn btn-warning flex-fill w-100">
+                Cancelamentos realizados<h3>{totalCancelamentos}</h3>
+              </button>
+            </div>
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn btn-info flex-fill w-100">
+                Ganhos realizados<h3>{totalEmRealAtendidos}</h3>
+              </button>
+            </div>
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn bg-danger flex-fill w-100">
+                Custos de atendimentos<h3>FAZER ...</h3>
+              </button>
+            </div>
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn bg-secondary flex-fill w-100">
+                Serviços a fazer<h3>{totalAgendado}</h3>
+              </button>
+            </div>
+            <div className="col-sm-6 mb-2">
+              <button type="button" className="btn bg-secondary flex-fill w-100">
+                Ganhos esperados<h3>{totalEmRealAgendado}</h3>
+              </button>
+            </div>
           </div>
         </div>
       </section>
