802ab2723a061dd0221ce2597fe77f5464d135d9
Marcus <alpande_1904@hotmail.com>
2024-03-30 10:20:26 -0300

    show clientes funcionando

--- src/components/CadastrarCliente/cadastrarCliente.jsx
+++ src/components/CadastrarCliente/cadastrarCliente.jsx
@@ -13,86 +13,104 @@ const SaveCadastroClienteModal = ({ open, onClose }) => {
   const [email, setEmail] = useState('');
 
   const handleSave = () => {
+    if (!nomeCliente || !telefone || !email || !cidade || !bairro || !rua || !numero) {
+      console.error('Por favor, preencha todos os campos obrigatórios.');
+      return;
+    }
     // Envie os dados para o servidor
     api.post('/salvar-cliente', {
       nomeCliente,
-        telefone,
-        cidade,
-        bairro,
-        rua,
-        numero,
-        email
-            })  
-            .then((response) => {
-                console.log('Dados salvos com sucesso!', response.data);
-                // Feche o modal após salvar os dados
-                onClose();
-           })
-            .catch((error) => {
-                 console.error('Erro ao salvar os dados:', error);
-            });
-    };
+      telefone,
+      cidade,
+      bairro,
+      rua,
+      numero,
+      email
+    })  
+    .then((response) => {
+      console.log('Dados salvos com sucesso!', response.data);
+      // Feche o modal após salvar os dados
+      handleCloseModal();
+    })
+    .catch((error) => {
+      console.error('Erro ao salvar os dados:', error);
+    });
+  };
+
+  const handleCloseModal = () => {
+    // Limpar os campos ao fechar o modal
+    setNomeCliente('');
+    setTelefone('');
+    setCidade('');
+    setBairro('');
+    setRua('');
+    setNumero('');
+    setEmail('');
+    // Chamar onClose para fechar o modal
+    onClose();
+  };
 
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby="simple-modal-title"
-      aria-describedby="simple-modal-description"
-    >
-      <div className="paper">
-        <h2 id="simple-modal-title">Cadastrar Cliente</h2>
-        <form>
-          <TextField
-            label="Nome do Cliente"
-            type="string"
-            value={nomeCliente}
-            onChange={(e) => setNomeCliente(e.target.value)}
-            fullWidth
-            TextField=""
-          />
-          <TextField
-            label="Telefone"
-            type="string"
-            value={telefone}
-            onChange={(e) => setTelefone(e.target.value)}
-            fullWidth
-          />
-         <TextField
-            label="Cidade"
-            value={cidade}
-            onChange={(e) => setCidade(e.target.value)}
-            fullWidth
-          />
-          <TextField
-            label="Bairro"
-            type="string"
-            value={bairro}
-            onChange={(e) => setBairro(e.target.value)}
-            fullWidth
-          />
-          <TextField
-            label="Rua"
-            value={rua}
-            onChange={(e) => setRua(e.target.value)}
-            fullWidth
-          />
+    <>
+      <Modal
+        open={open}
+        onClose={handleCloseModal} // Alteração aqui para chamar a função de fechar com limpeza
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div className="paper">
+          <h2 id="simple-modal-title">Cadastrar Cliente</h2>
+          <form>
             <TextField
-                label="Número"
-                value={numero}
-                onChange={(e) => setNumero(e.target.value)}
-                fullWidth
+              label="Nome do Cliente"
+              type="string"
+              value={nomeCliente}
+              onChange={(e) => setNomeCliente(e.target.value)}
+              fullWidth
             />
             <TextField
-                label="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                fullWidth
+              label="Telefone"
+              type="string"
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+              fullWidth
             />
-          <Button variant="contained" onClick={handleSave}>Salvar</Button>
-        </form>
-      </div>
-    </Modal>
+            <TextField
+              label="Cidade"
+              value={cidade}
+              onChange={(e) => setCidade(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              label="Bairro"
+              type="string"
+              value={bairro}
+              onChange={(e) => setBairro(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              label="Rua"
+              value={rua}
+              onChange={(e) => setRua(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              label="Número"
+              value={numero}
+              onChange={(e) => setNumero(e.target.value)}
+              fullWidth
+            />
+            <TextField
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" onClick={handleSave}>Salvar</Button>
+          </form>
+        </div>
+      </Modal>
+    </>
   );
 };
 
--- src/components/modalEditarCliente/modalEditarCliente.jsx
+++ src/components/modalEditarCliente/modalEditarCliente.jsx
@@ -1,7 +1,4 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
+import React, { useState, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,201 +6,94 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import api from "../../services/api.js";
 
-function ListaClientees() {
-  const [servicoDados, setServicoDados] = useState([]);
+export default function TabelaClientes() {
+  const [termoBusca, setTermoBusca] = useState('');
+  const [pagina, setPagina] = useState(1);
+  const [informacao, setInformacao] = useState('Dados dos Clientes');
+  const registrosPorPagina = 4;
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [rows, setRows] = useState([]);
 
-  useEffect(() => { api.get(`/busca-atendimento-realizado`)
-            .then((response) => {
-                setServicoDados(response.data);
-            })
-            .catch((err) => {
-                console.error("Erro ao buscar os dados:", err);
-            });
-          
-        },[]);
-}
+  useEffect(() => {
+    async function fetchClientes() {
+      try {
+        const response = await api.get(`/busca-atendimento-realizado`);
+        setRows(response.data);
+        setFilteredRows(response.data); // Inicialmente, todas as linhas são mostradas
+      } catch (err) {
+        console.error("Erro ao buscar os dados:", err);
+      }
+    }
 
+    fetchClientes();
+  }, []);
 
-
-function createData(name, endereco, contato, lastService, valor, price) {
-
-
-  return {
-    name,
-    endereco,
-    contato,
-    lastService,
-    valor,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        typeService: '11091700',
-        amount: 3,
-        serviceValor: 3.99,
-      },
-      {
-        date: '2020-01-02',
-        typeService: 'Anonymous',
-        amount: 1,
-        serviceValor: 99.99,
-
-      },
-    ],
-  };
-}
-
-const CustomWhatsAppIcon = () => (
-  <IconButton
-    aria-label="whatsapp"
-    onClick={() => {
-      window.location.href = 'http://wa.me/5553981578125';
-    }}
-  >
-    <WhatsAppIcon />
-  </IconButton>
-);
-
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.endereco}</TableCell>
-        <TableCell align="right">{row.contato}</TableCell>
-        <TableCell align="right">{row.lastService}</TableCell>
-        <TableCell align="right">{row.valor}</TableCell>
-        <TableCell align="right">{row.price}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Data Serviço</TableCell>
-                    <TableCell>Tipo</TableCell>
-                    <TableCell align="right">Quantidade</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.typeService}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * historyRow.serviceValor * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, CustomWhatsAppIcon(), '2023-03-11', 3.99),
-  createData('Ice cream sandwich', 237, CustomWhatsAppIcon(),'2023-07-11', 5 ),
-  createData('Eclair', 262, CustomWhatsAppIcon(), '2022-03-22', 3.79 ),
-  createData('Cupcake', 305, CustomWhatsAppIcon(), '2024-03-22', 2.5),
-  createData('Gingerbread', 356, CustomWhatsAppIcon(), '2022-03-22', 1.5),
-];
-
-export default function CollapsibleTable() {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [page, setPage] = React.useState(1);
-  const [filteredRows, setFilteredRows] = React.useState(rows);
-  const [information, setInformation] = React.useState('Dados dos Clientes');
-  const rowsPerPage = 4;
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleChangeBusca = (event) => {
+    setTermoBusca(event.target.value);
+    filterRows(event.target.value); // Filtro automático ao digitar
   };
 
-  const handlePageChange = (event, value) => {
-    setPage(value);
+  const handleChangePagina = (event, value) => {
+    setPagina(value);
   };
 
-  const handleAddOneYear = () => {
+  const filterRows = (searchTerm) => {
+    const filtered = rows.filter(
+      (row) =>
+        row.nome_cliente.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.rua.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        row.telefone.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredRows(filtered);
+    setPagina(1);
+  };
+
+  const filtrarDadosUmAno = () => {
     const today = new Date();
     const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
-    const filtered = rows.filter((row) => new Date(row.lastService) < oneYearAgo);
+    const filtered = rows.filter((row) => new Date(row.data_servico) < oneYearAgo);
     setFilteredRows(filtered);
-    setPage(1);
-    setInformation('Serviços realizados a + de 1 ano');
+    setPagina(1);
+    setInformacao('Serviços realizados a + de 1 ano');
   };
 
-  const handleAddSixMonths = () => {
+  const filtrarDadosSeisMeses = () => {
     const today = new Date();
     const sixMonthsAgo = new Date(today.getFullYear(), today.getMonth() - 6, today.getDate());
     const oneYearAgo = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
-    const filtered = rows.filter((row) => new Date(row.lastService) < sixMonthsAgo && new Date(row.lastService) > oneYearAgo );
+    const filtered = rows.filter((row) => new Date(row.data_servico) < sixMonthsAgo && new Date(row.data_servico) > oneYearAgo);
     setFilteredRows(filtered);
-    setPage(1);
-    setInformation('Serviço realizado entre 6 meses e 1 ano');
+    setPagina(1);
+    setInformacao('Serviço realizado entre 6 meses e 1 ano');
   };
 
-  const handleRemoveFilters = () => {
+  const removerFiltros = () => {
     setFilteredRows(rows);
-    setPage(1);
-    setInformation('Dados dos clienetes');
+    setPagina(1);
+    setInformacao('Dados dos clientes');
   };
 
-  const filteredPaginatedRows = filteredRows.filter((row) =>
-    row.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const startIndex = (page - 1) * rowsPerPage;
-  const endIndex = startIndex + rowsPerPage;
-  const paginatedRows = filteredPaginatedRows.slice(startIndex, endIndex);
+  const indiceInicio = (pagina - 1) * registrosPorPagina;
+  const indiceFim = indiceInicio + registrosPorPagina;
+  const registrosPaginados = filteredRows.slice(indiceInicio, indiceFim);
 
   return (
     <div>
       <TableContainer component={Paper}>
         <TextField
-          id="search"
-          label="Buscar Nome"
-          value={searchTerm}
-          onChange={handleSearchChange}
+          id="busca"
+          label="Buscar Nome, Endereço ou Contato"
+          value={termoBusca}
+          onChange={handleChangeBusca}
           sx={{
             mx: 'auto',
             my: 2,
@@ -212,33 +102,38 @@ export default function CollapsibleTable() {
             backgroundColor: '#ffffff'
           }}
         />
-        <div style={{ textAlign: 'center' }} id="informacao"><strong>{information}</strong></div>
-        <Table aria-label="collapsible table">
+        <div style={{ textAlign: 'center' }} id="informacao"><strong>{informacao}</strong></div>
+        <Table aria-label="tabela expansível">
           <TableHead>
             <TableRow>
-              <TableCell />
               <TableCell>Cliente</TableCell>
               <TableCell align="right">Endereço</TableCell>
               <TableCell align="right">Contato</TableCell>
-              <TableCell align="right">Ultimo Serviço</TableCell>
+              <TableCell align="right">Último Serviço</TableCell>
               <TableCell align="right">Valor</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {paginatedRows.map((row) => (
-              <Row key={row.name} row={row} />
+            {registrosPaginados.map((row) => (
+              <TableRow key={row.nome_cliente}>
+                <TableCell>{row.nome_cliente}</TableCell>
+                <TableCell align="right">{row.rua}</TableCell>
+                <TableCell align="right">{row.telefone}</TableCell>
+                <TableCell align="right">{row.data_servico}</TableCell>
+                <TableCell align="right">{row.valor}</TableCell>
+              </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
       <Stack spacing={2} sx={{ justifyContent: 'center', mt: 2 }}>
-        <Button variant="contained" onClick={handleAddOneYear}>+1 ano</Button>
-        <Button variant="contained" onClick={handleAddSixMonths}>+6 meses</Button>
-        <Button variant="contained" onClick={handleRemoveFilters}>Retirar Filtros</Button>
+        <Button variant="contained" onClick={filtrarDadosUmAno}>+1 ano</Button>
+        <Button variant="contained" onClick={filtrarDadosSeisMeses}>+6 meses</Button>
+        <Button variant="contained" onClick={removerFiltros}>Retirar Filtros</Button>
         <Pagination
-          count={Math.ceil(filteredPaginatedRows.length / rowsPerPage)}
-          page={page}
-          onChange={handlePageChange}
+          count={Math.ceil(filteredRows.length / registrosPorPagina)}
+          page={pagina}
+          onChange={handleChangePagina}
           color="primary"
         />
       </Stack>
